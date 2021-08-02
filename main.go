d70b95910cd58dac3d2a5da90cff7b3b97345f93
kynmh69 <hiroki.kayanuma@gmail.com>
2021-08-02 18:28:32 +0900

    mod

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"test_api/model"
 )
 
 func main() {
 	fmt.Println("Hello world.")
 	result := 1 + 1
-	fmt.Printf("result: %d", result)
-
+	fmt.Printf("result: %d\n", result)
+	p_slice := [...]*model.Person{model.New("hiroki", "kayanuma", 25), model.New("Shiho", "Ito", 24)}
+	for _, p := range p_slice {
+		p.Greetings()
+	}
+	fmt.Printf("type: %T, value: %v\n", p_slice, p_slice)
 }
--- model/person.go
+++ model/person.go
@@ -1,7 +1,27 @@
 package model
 
+import "fmt"
+
 type Person struct {
-	userName string
-	password string
-	age      string
+	firstName string
+	lastName  string
+	password  string
+	age       int
+}
+
+func New(firstName string, lastName string, age int) *Person {
+	person := new(Person)
+	person.firstName = firstName
+	person.lastName = lastName
+	person.age = age
+	person.password = ""
+	return person
+}
+
+func (p *Person) GetFullName() string {
+	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
+}
+
+func (p *Person) Greetings() {
+	fmt.Printf("Hello. %s\n", p.GetFullName())
 }
